f6198bc2b300075d331c72ab1f81f5592fe497d4
Florencia Stranges <florstranges@hotmail.com>
2020-12-03 20:39:20 -0300

    Nuevo navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css';
+
+let Navbar = () => 
+<div>
+    <header className="navbar">
+        <div>
+            <h1>Taverna de Moe</h1>
+        </div>
+        <nav className="nav">
+            <ul>
+                <li><a href="">Aperitivos</a></li>
+                <li><a href="">Cervezas</a></li>
+                <li><a href="">Espumantes</a></li>
+                <li><a href="">Licores</a></li>
+                <li><a href="">Gin</a></li>
+            </ul>
+        </nav>
+    </header>
+</div>;
+
+export default Navbar;
